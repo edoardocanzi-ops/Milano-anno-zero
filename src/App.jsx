1b73e24d99b26bc8eff05f8425835c097b202082
edoardocanzi-ops <edoardo.canzi@gmail.com>
2026-03-31 20:46:20 +0200

    Update image URLs for sport bags and backpacks

--- src/App.jsx
+++ src/App.jsx
@@ -24,11 +24,11 @@ const CUSTOM_IMAGES = {
   'Mazza da Baseball': '', 
 
   // --- ZAINI ---
-  'Sacca Sportiva': 'https://i.postimg.cc/CCwx9TFJ/Screenshot_20260330_142331_2.jpg',
+  'Sacca Sportiva': 'https://i.postimg.cc/cCws9TFJ/Screenshot_20260330_142331_2.jpg',
   'Sacca a Tracolla': 'https://i.postimg.cc/cCcSVYjq/Screenshot_20260330_142331_3.jpg',
   'Zaino da Escursionismo': 'https://i.postimg.cc/T1xd7QtW/Screenshot_20260330_142331_4.jpg',
-  'Zaino Tattico Militare': 'https://i.postimg.cc/D0hvj5xZ/Screenshot_20260330_142331_6.jpg',
-  'Zaino Sopravvivenza': 'https://i.postimg.cc/cCcSVYjq/Screenshot_20260330_142331_3.jpg',
+  'Zaino Tattico Militare': 'https://i.postimg.cc/h48PzWGP/Screenshot_20260330_142331_5.jpg',
+  'Zaino Sopravvivenza': 'https://i.postimg.cc/D0hvj5xZ/Screenshot_20260330_142331_6.jpg', 
 
   // --- SET 1: CASUAL (Comune) ---
   'Berretto di Lana': 'https://i.postimg.cc/3Nh8S9tj/Screenshot_20260330_132744_2.jpg',
